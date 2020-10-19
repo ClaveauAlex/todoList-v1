298dcb0fae5226f3b5c804e8c256a5d4052e858f
Alexandre Claveau <a.claveau29@gmail.com>
2020-10-19 17:15:27 +0200

    Our first template with EJS

--- app.js
+++ app.js
@@ -2,9 +2,48 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
+app.set('view engine', 'ejs');
 
 app.get("/",function (req,res) {
-    res.send("HelloWorld");
+    var today = new Date();
+    var currentDay = today.getDay()
+    var day = "";
+    if (currentDay === 6 | currentDay === 0) { //Saturday or Sunday
+        day = "Weekend";
+    }else{
+        day = "Weekday"
+    }
+
+    switch (currentDay) {
+        case 0:
+            day = "Sunday";
+            break;
+        case 1:
+            day = "Monday";
+            break;
+        case 2:
+            day = "Tuesday";
+            break;
+        case 3:
+            day = "Wednesday";
+            break;
+        case 4:
+            day = "Thursday";
+            break;
+        case 5:
+            day = "Friday";
+            break;
+        case 6:
+            day = "Saturday";
+            break;
+        default:
+            console.log("Error: current day is equal to : "+currentDay);
+            break;
+    }
+
+    res.render("list",{
+        kindOfDay: day
+    });
 })
 
 app.listen(3000,function () {
