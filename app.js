d39733a5885555c027ab9e992166c442931ca298
Alexandre Claveau <a.claveau29@gmail.com>
2020-10-19 18:24:57 +0200

    Passing Data from your Webpage to your server

--- app.js
+++ app.js
@@ -2,48 +2,30 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
+var items = ["Buy Food","Eat Food","Sleep"];
+
 app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.get("/",function (req,res) {
     var today = new Date();
-    var currentDay = today.getDay()
-    var day = "";
-    if (currentDay === 6 | currentDay === 0) { //Saturday or Sunday
-        day = "Weekend";
-    }else{
-        day = "Weekday"
+
+    var options = {
+        weekday:"long",
+        day:"numeric",
+        month:"long"
     }
 
-    switch (currentDay) {
-        case 0:
-            day = "Sunday";
-            break;
-        case 1:
-            day = "Monday";
-            break;
-        case 2:
-            day = "Tuesday";
-            break;
-        case 3:
-            day = "Wednesday";
-            break;
-        case 4:
-            day = "Thursday";
-            break;
-        case 5:
-            day = "Friday";
-            break;
-        case 6:
-            day = "Saturday";
-            break;
-        default:
-            console.log("Error: current day is equal to : "+currentDay);
-            break;
-    }
+    var day = today.toLocaleDateString("en-US",options);
 
-    res.render("list",{
-        kindOfDay: day
-    });
+    res.render("list",{kindOfDay: day,newListItems:items});
+})
+
+app.post("/",function (req,res) {
+    var item = req.body.newItem; 
+    items.push(item);
+    res.redirect("/");   
 })
 
 app.listen(3000,function () {
