92eedceca39d297f10fa753ec92347b8b5a8af38
Alexandre Claveau <a.claveau29@gmail.com>
2020-10-21 12:07:42 +0200

    Refactoring code

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname+"/date.js");
 
 const app = express();
-let items = ["Buy Food","Eat Food","Sleep"];
+const items = ["Buy Food","Eat Food","Sleep"];
+const workItems = [];
 
 app.set('view engine', 'ejs');
 
@@ -10,23 +12,33 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
 
 app.get("/",function (req,res) {
-    let today = new Date();
+    const day = date.getDate(); //Permet de chercher la fonction getDate() dans le fichier date.js
 
-    let options = {
-        weekday:"long",
-        day:"numeric",
-        month:"long"
-    }
-
-    let day = today.toLocaleDateString("en-US",options);
-
-    res.render("list",{kindOfDay: day,newListItems:items});
+    res.render("list",{listTitleDay: day,newListItems:items});
 })
 
 app.post("/",function (req,res) {
+    const item = req.body.newItem; 
+    if (req.body.list === "Work") {
+        workItems.push(item);
+        res.redirect("/work"); 
+    }else{
+        items.push(item);
+        res.redirect("/"); 
+    }
+})
+
+app.get("/work",function (req,res) {
+    res.render("list",{
+        listTitleDay:"Work List",
+        newListItems: workItems
+    }); 
+})
+
+app.post("/work ",function (req,res) {
     let item = req.body.newItem; 
-    items.push(item);
-    res.redirect("/");   
+    workItems.push(item);
+    res.redirect("/work");   
 })
 
 app.listen(3000,function () {
---
+++ date.js
@@ -0,0 +1,16 @@
+//module.exports est identique avec juste exports
+exports.getDate = function() {
+    const today = new Date();
+
+    const options = {weekday:"long",day:"numeric",month:"long"}
+
+    return today.toLocaleDateString("en-US",options);
+}
+
+exports.getDay = function() {
+    const today = new Date();
+
+    const options = {weekday:"long",}
+
+    return today.toLocaleDateString("en-US",options);
+}
